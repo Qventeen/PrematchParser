94bf86e708aa8d40bb90b30382fdcbb817c157fa
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-04 21:08:39 +0300

    Implement main async logic

--- src/main/java/org/serhiituhaienko/App.java
+++ src/main/java/org/serhiituhaienko/App.java
@@ -1,55 +1,76 @@
 package org.serhiituhaienko;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.serhiituhaienko.models.Sport;
+import org.serhiituhaienko.models.*;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class App {
-    public static void main( String[] args ) throws IOException, InterruptedException {
+    private static final String SPORT_URI = "https://leonbets.com/api-2/betline/sports?ctag=en-US&flags=urlv2";
+    private static final String LEAGUE_URI_TEMPLATE = "https://leonbets.com/api-2/betline/events/all?ctag=en-US&league_id=%s&hideClosed=true&flags=reg,urlv2,mm2,rrc,nodup";
+    private static final String MATCH_URI_TEMPLATE = "https://leonbets.com/api-2/betline/event/all?ctag=en-US&eventId=%s&flags=smg,reg,urlv2,mm2,rrc,nodup";
+
+    public static void main( String[] args ) {
         HttpClient client = HttpClient.newHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        //0-request
-        //test getting http://leonbets.com
 
+        List<Sport> sportList = fromJson(getAsync(client, SPORT_URI).join().body(), new TypeReference<>() {});
+        List<Sport.League> topLeagues = sportList.stream()
+                .flatMap(sport -> sport.regions().stream())
+                .flatMap(region -> region.leagues().stream())
+                .filter(Sport.League::top)
+                .toList();
 
-        //1-request
-        //get main data about sports
-        //https://leonbets.com/api-2/betline/sports?ctag=en-US&flags=urlv2
+        Map<String, List<League.Match>> matches = new ConcurrentHashMap<>();
 
+        for (Sport.League league : topLeagues) {
+            CompletableFuture.allOf(getAsync(client, String.format(LEAGUE_URI_TEMPLATE, league.id()))
+                    .thenApply(HttpResponse::body)
+                    .thenApply(json -> fromJson(json, new TypeReference<League>() {}))
+                    .thenApply(League::matches)
+                    .thenApply(list -> matches.put(league.id(), list))).join();
+        }
 
-        String result = getData(client, "https://leonbets.com/api-2/betline/sports?ctag=en-US&flags=urlv2").body();
+        Map<String, List<Match.Market>> markets = new ConcurrentHashMap<>();
 
-        List<Sport> sportList = objectMapper.readValue(result, new TypeReference<>() {});
-        System.out.println(sportList);
-
-        //2-request
-        //get data about concrete league
-        //https://leonbets.com/api-2/betline/events/all?ctag=en-US&league_id=1970324836980105&hideClosed=true&flags=reg,urlv2,mm2,rrc,nodup
-        //System.out.println(getData(client, "https://leonbets.com/api-2/betline/events/all?ctag=en-US&league_id=1970324836980105&hideClosed=true&flags=reg,urlv2,mm2,rrc,nodup").body());
-        //3-request
-        //for every event in league
-        //https://leonbets.com/api-2/betline/event/all?ctag=en-US&eventId=1970324842107886&flags=smg,reg,urlv2,mm2,rrc,nodup
-//        System.out.println(getData(client, "https://leonbets.com/api-2/betline/events/all?ctag=en-US&league_id=1970324836980105&hideClosed=true&flags=reg,urlv2,mm2,rrc,nodup").body());
-
-
+        for (League.Match match : matches.values().stream().flatMap(List::stream).toList()) {
+            CompletableFuture.allOf(getAsync(client, String.format(MATCH_URI_TEMPLATE, match.id()))
+                    .thenApply(HttpResponse::body)
+                    .thenApply(json -> fromJson(json, new TypeReference<Match>() {}))
+                    .thenApply(Match::markets)
+                    .thenApply(list -> markets.put(match.id(), list))).join();
+        }
     }
 
-    private static HttpResponse<String> getData(HttpClient client, String uri) throws IOException, InterruptedException {
+    private static CompletableFuture<HttpResponse<String>> getAsync(HttpClient client, String uri) {
         HttpRequest request = HttpRequest
                 .newBuilder(URI.create(uri))
                 .header("accept", "application/json")
                 .build();
 
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private static <T> T fromJson(String data, TypeReference<T> type) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        T result;
+        try {
+            result = objectMapper.readValue(data, type);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
