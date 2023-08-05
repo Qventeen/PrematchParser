08ef0c75e67151b282f0771fa207ab4d6edcf403
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-05 14:21:59 +0300

    Fix date format

--- src/main/java/org/serhiituhaienko/util/Printer.java
+++ src/main/java/org/serhiituhaienko/util/Printer.java
@@ -3,19 +3,24 @@ package org.serhiituhaienko.util;
 import org.serhiituhaienko.models.Match;
 import org.serhiituhaienko.models.Sport;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.System.out;
 
 public class Printer {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'").withZone(ZoneId.of("UTC"));
+
     public static void print(List<Sport> sports, Map<String, Match> matches) {
+
         var space = ' ';
-        for (var sport: sports) {
-            for (var league: sport.topLeagues()) {
+        for (var sport : sports) {
+            for (var league : sport.topLeagues()) {
                 out.printf("%s, %s %s%n", sport.name(), league.regionName(), league.name());
                 var match = matches.get(league.id());
-                out.printf("%5s%s, %s, %s%n", space, match.name(), match.startTime(), match.id());
+                out.printf("%5s%s, %s, %s%n", space, match.name(), FORMATTER.format(match.startTime()), match.id());
                 for (var market : match.markets()) {
                     out.printf("%10s%s%n", space, market.name());
                     for (var runner : market.runners()) {
